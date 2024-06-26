4b5f1d6d6da51564e1efef39be4b385604888ff3
Wanderson Santos <wandersonsg10@gmail.com>
2024-06-26 16:10:50 -0300

    inclusão do contador e aos

---
+++ src/scripts/main.js
@@ -0,0 +1,37 @@
+AOS.init();
+
+const dataDoEvento = new Date("Jul 24, 2024 00:00:01");
+const timesTampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timesTampDoEvento - timeStampAtual;
+
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+
+
+    const diasAteOEvento =  Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    console.log (distanciaAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento estreado no Breideflix'
+    }
+
+
+},1000);
